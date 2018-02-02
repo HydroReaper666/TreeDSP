2085389e1aa8e68b48b8b796c42a4f58c74eef0b
MerryMage <MerryMage@users.noreply.github.com>
2018-02-02 19:01:59 +0000

    Rename to tdsp-sender

--- tdsp-sender/main.cpp
+++ tdsp-sender/main.cpp
@@ -4,11 +4,32 @@
 #include <sstream>
 #include <variant>
 
+#include <boost/asio.hpp>
+
 #include "asm_lexer.h"
 #include "asm_parse.h"
 #include "instruction_table_lexer.h"
 
-int main() {
+using boost::asio::ip::udp;
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        printf("Usage: program <host> <port>");
+    }
+
+    boost::asio::io_service io_service;
+    udp::socket socket(io_service, udp::endpoint(udp::v4(), 0));
+
+    udp::endpoint endpoint = [&] {
+        const std::string host = argv[1];
+        const std::string port = argv[2];
+
+        udp::resolver resolver(io_service);
+        udp::resolver::query query(udp::v4(), host, port);
+        udp::resolver::iterator iter = resolver.resolve(query);
+        return *iter;
+    }();
+
     auto table = BuildParserTable();
 
     AsmLexer lexer{std::cin};
@@ -48,6 +69,8 @@ int main() {
 
                 std::vector<std::uint16_t> message = *result;
                 message.insert(message.begin(), 0xD590);
+
+                socket.send_to(boost::asio::buffer(message), endpoint);
 
                 break;
             }
